55bf4c29234705898e6122403780068a8cc30131
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-10-10 21:20:23 -0300

    feat: turning light mode on only when audio hits a certain time

--- src/components/ThemeSwitcher/index.tsx
+++ src/components/ThemeSwitcher/index.tsx
@@ -4,29 +4,67 @@ import { RiMoonLine, RiSunLine } from 'react-icons/ri';
 import { useTheme } from 'styled-components';
 import * as S from './styles';
 
+const audios = [
+  '/audio/t_flashbang01.mp3',
+  '/audio/t_flashbang03.mp3',
+  '/audio/t_flashbang04.mp3',
+  '/audio/t_flashbang05.mp3',
+];
+
+type BangTimes = Record<
+  | 't_flashbang01.mp3'
+  | 't_flashbang03.mp3'
+  | 't_flashbang04.mp3'
+  | 't_flashbang05.mp3',
+  number
+>;
+
+const bangTimes: BangTimes = {
+  't_flashbang01.mp3': 640,
+  't_flashbang03.mp3': 990,
+  't_flashbang04.mp3': 1010,
+  't_flashbang05.mp3': 2140,
+};
+
 export function ThemeSwitcher() {
+  const [canPlay, setCanPlay] = React.useState(false);
+
   const theme = useTheme();
 
-  const audios = [
-    '/audio/t_flashbang01.wav',
-    '/audio/t_flashbang03.wav',
-    '/audio/t_flashbang04.wav',
-  ];
-
-  const audio =
-    typeof Audio !== 'undefined'
+  const audio = React.useMemo(() => {
+    return typeof Audio !== 'undefined'
       ? new Audio(audios[Math.floor(Math.random() * audios.length)])
       : undefined;
+  }, []);
 
   const { isDarkMode, disable, enable } = useDarkMode();
 
   const toggleLightMode = () => {
-    audio?.play();
+    if (canPlay) audio?.play();
+
+    let time = 0;
+    (Object.keys(bangTimes) as (keyof typeof bangTimes)[]).forEach(
+      (fileName) => {
+        if (audio?.src?.endsWith(fileName)) time = bangTimes[fileName];
+      },
+    );
 
     setTimeout(() => {
       disable();
-    }, 850);
+    }, time);
   };
+
+  React.useEffect(() => {
+    function enableAudio() {
+      setCanPlay(true);
+    }
+
+    audio?.addEventListener('canplaythrough', enableAudio);
+
+    return () => {
+      audio?.removeEventListener('canplaythrough', enableAudio);
+    };
+  }, [audio]);
 
   return (
     <S.Container>
