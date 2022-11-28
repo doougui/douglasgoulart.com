3a881e9b3dd72cea749cfd9fc875d37ab9cf2170
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-11-28 20:27:48 -0300

    tests: ensuring that Input component works properly

--- src/components/Input/test.tsx
+++ src/components/Input/test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from 'utils/tests';
+import { render, screen, waitFor } from 'utils/tests';
+import userEvent from '@testing-library/user-event';
 
 import { Input } from '.';
 
@@ -8,5 +9,45 @@ describe('<Input />', () => {
 
     expect(screen.getByPlaceholderText('input')).toBeInTheDocument();
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render with error', () => {
+    render(<Input name="test" hasError />);
+
+    expect(screen.getByRole('textbox')).toHaveStyleRule(
+      'border',
+      '1px solid var(--color-danger)',
+    );
+  });
+
+  it('changes its value when typing', async () => {
+    const onInputChange = jest.fn();
+    render(<Input onInputChange={onInputChange} />);
+
+    const input = screen.getByRole('textbox');
+    const text = 'This is my new text';
+    userEvent.type(input, text);
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text);
+      expect(onInputChange).toHaveBeenCalledTimes(text.length);
+    });
+    expect(onInputChange).toHaveBeenCalledWith(text);
+  });
+
+  it('does not change its value when disabled', async () => {
+    const onInputChange = jest.fn();
+    render(<Input onInputChange={onInputChange} disabled />);
+
+    const input = screen.getByRole('textbox');
+    expect(input).toBeDisabled();
+
+    const text = 'This is my new text';
+    userEvent.type(input, text);
+
+    await waitFor(() => {
+      expect(input).not.toHaveValue(text);
+    });
+    expect(onInputChange).not.toHaveBeenCalled();
   });
 });
