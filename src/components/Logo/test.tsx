8a0dd8d6fe1c5b139a37514acc90c75a92dacfa5
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-11-28 20:39:06 -0300

    tests: ensuring that the logo renders correctly with the light theme

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,11 +1,30 @@
-import { render } from 'utils/tests';
+import { ColorThemeProvider } from 'contexts/ColorThemeContext';
+import { render, screen } from 'utils/tests';
 
 import { Logo } from '.';
 
 describe('<Logo />', () => {
-  it('should render correctly', () => {
+  it('should render the logo', () => {
     const { container } = render(<Logo />);
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders with dark theme by default', () => {
+    render(<Logo />);
+
+    expect(screen.getByTestId('name')).toHaveAttribute('fill', '#EDEDEF');
+    expect(screen.getByTestId('dot')).toHaveAttribute('fill', '#107BEB');
+  });
+
+  it('renders with light theme', () => {
+    render(
+      <ColorThemeProvider initialTheme="light">
+        <Logo />
+      </ColorThemeProvider>,
+    );
+
+    expect(screen.getByTestId('name')).toHaveAttribute('fill', '#1A1523');
+    expect(screen.getByTestId('dot')).toHaveAttribute('fill', '#107BEB');
   });
 });
