6432f12f5e6ba7bcef4afc7c2e12b7060c9adf2d
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2023-04-20 22:40:11 -0300

    fix: fixing api error when not playing spotify song

--- src/pages/api/spotify/nowPlaying.ts
+++ src/pages/api/spotify/nowPlaying.ts
@@ -27,14 +27,14 @@ export default async function handler(_: NextApiRequest, res: NextApiResponse) {
   }
 
   const song: CurrentlyPlayingResponse = response;
-  const isPlaying = song.is_playing;
-  const title = song.item.name;
-  const artist = song.item.artists
+  const isPlaying = song?.is_playing;
+  const title = song.item?.name;
+  const artist = song.item?.artists
     .map((artistItem) => artistItem.name)
     .join(', ');
-  const album = song.item.album.name;
-  const albumImageUrl = song.item.album.images[0].url;
-  const songUrl = song.item.external_urls.spotify;
+  const album = song.item?.album.name;
+  const albumImageUrl = song.item?.album.images[0].url;
+  const songUrl = song.item?.external_urls.spotify;
 
   return res.status(200).json({
     album,
