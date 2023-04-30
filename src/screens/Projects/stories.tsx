68ce7c4f07ee9d2e28d9155ecf75bcb5e88013aa
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2023-04-30 16:25:46 -0300

    tests: making sure the projects page is working properly

--- src/screens/Projects/stories.tsx
+++ src/screens/Projects/stories.tsx
@@ -1,9 +1,10 @@
 import type { Meta, StoryFn } from '@storybook/react';
 import { Projects } from '.';
+import projectsMock from './mock';
 
 export default {
   title: 'Projects',
   component: Projects,
 } as Meta;
 
-export const Default: StoryFn = () => <Projects />;
+export const Default: StoryFn = () => <Projects projects={projectsMock} />;
--- src/screens/Projects/test.tsx
+++ src/screens/Projects/test.tsx
@@ -1,4 +1,5 @@
-import { render } from 'utils/tests';
+import { render, screen } from 'utils/tests';
+import projectsMock from './mock';
 
 import { Projects } from '.';
 
@@ -12,8 +13,29 @@ useRouter.mockImplementation(() => ({
   route: '/',
 }));
 
+jest.mock('components/Project', () => ({
+  __esModule: true,
+  Project: () => <div data-testid="project" />,
+}));
+
 describe('<Projects />', () => {
-  it('should render correctly', () => {
-    render(<Projects />);
+  it('should render heading and projects', () => {
+    const { container } = render(<Projects projects={projectsMock} />);
+
+    expect(
+      screen.getByRole('heading', { name: /Projects/i }),
+    ).toBeInTheDocument();
+
+    expect(screen.getAllByTestId('project')).toHaveLength(3);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render "See all projects" button', () => {
+    render(<Projects projects={projectsMock} />);
+
+    expect(
+      screen.getByRole('link', { name: /See all my projects/i }),
+    ).toHaveAttribute('href', 'https://github.com/doougui');
   });
 });
