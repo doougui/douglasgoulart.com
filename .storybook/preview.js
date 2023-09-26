de5fcc743c61d6f980a61de326dbe1e0fc29e600
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2023-09-26 11:51:34 -0300

    Revert "upgrading storybook"
    This reverts commit f82d4a3703146b5ed47a1ea8a231a6095ba0c383.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,27 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from 'styles/global';
+import * as NextImage from 'next/image';
 import defaultTheme from 'styles/theme';
 import { RouterContext } from "next/dist/shared/lib/router-context";
 import { ColorThemeProvider } from 'contexts/ColorThemeContext';
 import { COLORS } from 'components/ColorTheme/colors';
+
+const OriginalNextImage = NextImage.default;
+
+/* eslint no-import-assign: "off" */
+/* eslint react/jsx-filename-extension: "off" */
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage
+      {...props}
+      unoptimized
+      width={0}
+      height={0}
+      blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAADAAQDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAbEAADAAMBAQAAAAAAAAAAAAABAgMABAURUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAFxEAAwEAAAAAAAAAAAAAAAAAAAECEf/aAAwDAQACEQMRAD8Anz9voy1dCI2mectSE5ioFCqia+KCwJ8HzGMZPqJb1oPEf//Z"
+    />
+  ),
+});
 
 export const parameters = {
   backgrounds: {
